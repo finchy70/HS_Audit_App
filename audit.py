810527342f90673d93ff8359a2905e3b0098c5ee
Finchy <finchy70@gmail.com>
2015-06-18 16:17:07 +0100

    Moved everything around and added som comments to tidy things up.  Removed som files I had been using to test stuff.

--- audit.py
+++ audit.py
@@ -1,12 +1,10 @@
 __author__ = 'Paul Finch'
 
-"""
-This piece of code sets up a frame to display the simple front menu
-"""
+#This piece of code sets up a frame to display the simple front menu
 import wx
 import sqlite3
 
-
+#This is the front main menu panel
 class FrontPanel(wx.Panel):
 
     def __init__(self, parent):
@@ -28,38 +26,53 @@ class FrontPanel(wx.Panel):
         main_sizer.AddStretchSpacer()
         self.SetSizer(main_sizer)
 
+        # This opens the Create New Audit frame.
     def create_audit(self, event):
         print 'create audit'
         self.Close(True)
         frame = FrontAudit()
         app.MainLoop()
 
-
+        # To be completed
     def create_engineer(self, event):
         option = 2
         print "Create engineer"
         exit()
 
+    # To be completed
     def view_audit(self, event):
         option = 3
         print "View Audit"
         exit()
 
+    # To be completed
     def close_app(self, event):
         option = 4
         exit('Good Bye')
 
 
-class MyFrame(wx.Frame):
+#This creates the panel for the Create New Audit Menu
+class CreateAudit(wx.Panel):
 
-    def __init__(self):
-        """Constructor"""
-        wx.Frame.__init__(self, None, title="")
-        panel = wx.Panel(self)
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.lblname = wx.StaticText(self, label="Site Name :", pos=(20,60))
+        self.sitename = wx.TextCtrl(self, value="Enter site name here.", pos=(150, 60), size=(140,-1))
+        self.lblname = wx.StaticText(self, label="4 Digit Job Number", pos=(20,120))
+        self.jobnumber = wx.TextCtrl(self, pos=(150, 120), size=(140,-1))
 
+        con = sqlite3.connect("hs_audit.sqlite")
+        cur = con.cursor()
+        con.row_factory = lambda cursor, row: row[0]
+        ids = con.execute('SELECT engineer FROM T1').fetchall()
+        myList = ids
+        self.lblname = wx.StaticText(self, label="Select Engineer :", pos=(20,180))
+        self.engineername = wx.ComboBox(self, pos=(150, 180), size=(140,-1)).SetItems(myList)
+
+        self.button =wx.Button(self, label="Save", pos=(150, 400))
         self.Show()
 
-
+#This creates the frame for the Main Menu
 class FrontFrame(wx.Frame):
     def __init__(self):
         """Constructor"""
@@ -69,6 +82,7 @@ class FrontFrame(wx.Frame):
         self.Centre()
         self.Show()
 
+#This creates the frame for the Create New Audit Menu.
 class FrontAudit(wx.Frame):
     def __init__(self):
         """Constructor"""
@@ -78,6 +92,8 @@ class FrontAudit(wx.Frame):
         self.Centre()
         self.Show()
 
+"""
+This section will manage all the questions via the sqlite3 database
 
 class Questions(object):
 
@@ -113,29 +129,8 @@ class HvAudit(Questions):
 
     def read_questions(self):
         pass
-
-
-class CreateAudit(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
-        self.lblname = wx.StaticText(self, label="Site Name :", pos=(20,60))
-        self.sitename = wx.TextCtrl(self, value="Enter site name here.", pos=(150, 60), size=(140,-1))
-        self.lblname = wx.StaticText(self, label="4 Digit Job Number", pos=(20,120))
-        self.jobnumber = wx.TextCtrl(self, pos=(150, 120), size=(140,-1))
-
-        con = sqlite3.connect("hs_audit.sqlite")
-        cur = con.cursor()
-        con.row_factory = lambda cursor, row: row[0]
-        ids = con.execute('SELECT engineer FROM T1').fetchall()
-        myList = ids
-        self.lblname = wx.StaticText(self, label="Select Engineer :", pos=(20,180))
-        self.engineername = wx.ComboBox(self, pos=(150, 180), size=(140,-1)).SetItems(myList)
-
-        self.button =wx.Button(self, label="Save", pos=(150, 400))
-        self.Show()
-
-
-
+"""
+#This kicks everything off.
 if __name__ == '__main__':
     app = wx.App(False)
     frame = FrontFrame()
